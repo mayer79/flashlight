7dd1feea09e6795f09ac15e43e39b185ae9e0289
mayer79 <mayermichael79@gmail.com>
2019-09-09 22:37:21 +0200

    first draft of breakdown

--- R/light_breakdown.R
+++ R/light_breakdown.R
@@ -1,24 +1,25 @@
-#' Variable Contribution Breakdown for Single Observations.
+#' Variable Contribution Breakdown for Single Observation.
 #'
 #' Calculates variable contribution breakdown for a single observation, see Gosiewska and Biecek [1]. For non-additive models, the order of the variables visited is relevant and is determined based on variable importance by default.
 #'
 #' The breakdown algorithm assigns the following attribution to the first variable (x): In \code{data}, all values in x are set to its value in the new observation. This will change the (weighted) average of predicitons on \code{data} by a certain amount. This amount measures the contribution of x on the prediction. If this algorithm would be repeated for all possible permutations of the input variables, the resulting attributions would equal SHAP values. Note that the minimum required elements in the (multi-) flashlight are "y", "predict_function", "model", "data" and "metrics". The latter two can also directly be passed to \code{light_breakdown}. Note that by default, no retransformation function is applied.
 #'
-#' @importFrom dplyr left_join bind_rows
-#' @importFrom stats ave
+#' @importFrom dplyr semi_join
+#' @importFrom MetricsWeighted weighted_mean
+#' @importFrom tidyr crossing
 #' @param x An object of class \code{flashlight} or \code{multiflashlight}.
 #' @param new_obs One single new observation to calculate variable attribution for. Needs to be of same structure as \code{data}.
 #' @param data An optional \code{data.frame}.
-#' @param metric An optional named list of length one with a metric as element used to evaluate variable importance if \code{order_by_importance = TRUE}. Defaults to the first metric in the flashlight. The metric needs to be a function with at least four arguments: actual, predicted, case weights w and \code{...}.
+#' @param by An optional vector of column names used to subset \code{data} to relevant rows with equal values in by-variables as \code{new_obs}.
 #' @param v Vector of variables to assess contribution for. Defaults to all variables in \code{data}.
 #' @param order_by_importance Logical flag indicated if \code{v} should be ordered by variable importance.
 #' @param n_max Maximum number of rows to consider. Use if \code{data} is large.
-#' @param seed An integer random seed used to shuffle rows if \code{n_max} is smaller than the number of rows in \code{data}.
-#' @param lower_is_better Logical flag indicating if lower values in the metric are better or not in order to assess variable importance (only used if \code{order_by_importance = TRUE}). If set to FALSE, the increase in metric is multiplied by -1.
+#' @param seed An integer random seed used to shuffle rows if \code{n_max} is smaller than the number of rows in \code{data}. Furthermore passed to \code{light_im}
 #' @param use_linkinv Should retransformation function be applied? Default is FALSE.
 #' @param value_name Column name in resulting \code{data} containing the variable attribution. Defaults to "value".
 #' @param label_name Column name in resulting \code{data} containing the label of the flashlight. Defaults to "label".
 #' @param variable_name Column name in resulting \code{data} containing the variable names. Defaults to "variable".
+#' @param descripion_name Column name in resulting \code{data} containing the description of the value in the explained observation. Defaults to "description".
 #' @param ... Further arguments passed to \code{light_performance} (only used if \code{order_by_importance = TRUE}).
 #' @return An object of class \code{light_breakdown}, \code{light} (and a list) with the following elements.
 #' \itemize{
@@ -27,6 +28,7 @@
 #'   \item \code{value_name} Same as input \code{value_name}.
 #'   \item \code{label_name} Same as input \code{label_name}.
 #'   \item \code{variable_name} Same as input \code{variable_name}.
+#'   \item \code{description_name} Same as input \code{description_name}.
 #' }
 #' @export
 #' @references [1] A. Gosiewska and P. Biecek (2019). IBREAKDOWN: Uncertainty of model explanations for non-additive predictive models. ArXiv. <https://arxiv.org/abs/1903.11420>.
@@ -36,8 +38,8 @@
 #' mod_full <- flashlight(model = fit_full, label = "full", data = iris, y = "Sepal.Length")
 #' mod_part <- flashlight(model = fit_part, label = "part", data = iris, y = "Sepal.Length")
 #' mods <- multiflashlight(list(mod_full, mod_part), by = "Species")
-#' light_importance(mod_full)
-#' light_importance(mods)
+#' light_breakdown(mod_full, new_obs = iris[1, ])$data
+#' light_breakdown(mods, new_obs = iris[1, ])$data
 #'
 #' ir <- iris
 #' ir$log_sl <- log(ir$Sepal.Length)
@@ -62,31 +64,76 @@ light_breakdown.default <- function(x, ...) {
 
 #' @describeIn light_breakdown Variable attribution to single observation for a flashlight.
 #' @export
-light_breakdown.flashlight <- function(x, new_obs, data = x$data, metric = x$metrics[1],
+light_breakdown.flashlight <- function(x, new_obs, data = x$data, by = x$by,
                                        v = NULL, order_by_importance = TRUE,
-                                       n_max = Inf, seed = NULL,
-                                       lower_is_better = TRUE, use_linkinv = FALSE,
+                                       n_max = Inf, seed = NULL, use_linkinv = FALSE,
                                        value_name = "value", label_name = "label",
-                                       variable_name = "variable", ...) {
+                                       variable_name = "variable",
+                                       description_name = "description", ...) {
   stopifnot(!is.null(new_obs),
-            !is.null(metric), length(metric) == 1L,
-            (n <- nrow(data)) >= 1L,
-            !anyDuplicated(c(by, "metric", value_name, label_name, variable_name)))
+            nrow(data) >= 1L,
+            sort(colnames(new_obs)) == sort(colnames(data)),
+            !anyDuplicated(c(value_name, label_name, variable_name, description_name)))
 
-  if (!is.null(seed)) {
-    set.seed(seed)
+  # Subset data to the correct "by" values
+  if (length(by)) {
+    data <- semi_join(data, new_obs, by = by)
   }
+  n <- nrow(data)
+  stopifnot(n >= 1L)
+
   # Subsample to n_max
   if (n > n_max) {
+    if (!is.null(seed)) {
+      set.seed(seed)
+    }
     data <- data[sample(n, min(n_max, n)), , drop = FALSE]
   }
+  # Update flashlight
+  x <- flashlight(x, data = data, by = NULL,
+                  linkinv = if (use_linkinv) x$linkinv else function(z) z)
 
-  if (is.null(v)) {
-    v <- setdiff(colnames(data), c(x$y, x$by))
+  # Calculate contributions
+  mean_pred <- function(x, data = x$data, w = x$w) {
+    weighted_mean(predict(x, data = data), w, na.rm = TRUE)
   }
 
-  out <- list(data = imp, by = by, value_name = value_name,
-              label_name = label_name, variable_name = variable_name)
+  baseline <- mean_pred(x)
+
+  # Determine v and its order
+  if (is.null(v)) {
+    v <- setdiff(colnames(data), c(x$y, by))
+  }
+  if (order_by_importance) {
+    ind_impact <- vapply(v, function(vi) {
+      data[[vi]] <- new_obs[[vi]];
+      (mean_pred(x = x, data = data) - baseline)^2
+    }, FUN.VALUE = 1.0)
+    v <- names(sort(-ind_impact))
+  }
+
+  # Calculate contributions
+  mean_pred_vector <- numeric(length(v))
+
+  for (i in seq_along(v)) {
+    data[[v[i]]] <- new_obs[[v[i]]]
+    mean_pred_vector[i] <- mean_pred(x, data = data)
+  }
+
+  # Combine results
+  all_preds <- c(baseline, diff(c(baseline, mean_pred_vector)))
+  full_path <- data.frame(row.names = NULL, stringsAsFactors = FALSE,
+    factor(c("intercept", v), levels = c("intercept", v)),
+    c("intercept", paste(v, format(t(new_obs[, v]), digits = 2), sep = " = ")),
+    cumsum(all_preds))
+  colnames(full_path) <- c(variable_name, description_name, value_name)
+  meta_info <- data.frame(x$label, new_obs[by])
+  colnames(meta_info) <- c(label_name, by)
+
+  out <- list(data = crossing(meta_info, full_path),
+              by = by, value_name = value_name,
+              label_name = label_name, variable_name = variable_name,
+              description_name = description_name)
   class(out) <- c("light_breakdown", "light", "list")
   out
 }
