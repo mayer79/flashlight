edc8d65eaca5ef08097cc779c095ae979f2ec1b5
mayer79 <mayermichael79@gmail.com>
2019-10-05 21:07:58 +0200

    removed unnecessary dependencies

--- ale_profile.R
+++ ale_profile.R
@@ -7,34 +7,34 @@ breaks <- NULL
 v <- "Petal.Length"
 evaluate_at <- if (!is.null(breaks)) midpoints(breaks) else
   auto_cut(data[[v]], n_bins = 5)$bin_means
+m <- length(evaluate_at)
+
 
 
 ale_core <- function(from, to) {
-  lookup <- setNames(data.frame(c("from", "to"), c(from, to)), c("key", v))
-
-  if (is.logical(to) || is.numeric(to)) {
+   if (is.numeric(to)) {
     .s <- data[[v]] >= from & data[[v]] <= to
   } else {
     .s <- data[[v]] %in% c(from, to)
   }
   dat_i <- data[.s, , drop = FALSE]
   if (nrow(dat_i) == 0L) {
-    return(data.frame(counts = 0, Petal.Length = NA))
+    return(NULL)
   }
-  dat_i <- light_ice(x, v = v, data = dat_i , by = x$by,
+  ice <- light_ice(x, v = v, data = dat_i, by = x$by,
                      evaluate_at = c(from, to), n_max = 100)$data
-  dat_i <- left_join(dat_i, lookup, by = v)
-  dat_i[[v]] <- NULL
-  dat_i <- spread(dat_i, key = "key", value = "value")
-  dat_i[["value"]] <- dat_i[["to"]] - dat_i[["from"]]
-  grouped_stats(dat_i, x = "value", w = x$w, by = x$by)
+  dat_to <- ice[ice[[v]] == to, ]
+  dat_from <- ice[ice[[v]] == from, ]
+  dat_to[["value"]] <- dat_to[["value"]] -
+    dat_from[["value"]][match(dat_to[["id"]], dat_from[["id"]])]
+  out <- grouped_stats(dat_to, x = "value", w = x$w, by = x$by)
+  out[[v]] <- to
+  out
 }
 
-m <- length(evaluate_at)
-ale <- Map(ale_core, from = evaluate_at[c(1, 1:(m - 1L))], to = evaluate_at)
-ale <- dplyr::bind_rows(ale)
-ale[[v]] <- evaluate_at
-ale[is.na(ale[["value"]]), "value"] <- 0
-ale[["value"]] <- if (length(x$by)) ave(ale[["value"]], x$by, FUN = cumsum) else cumsum(ale[["value"]])
-
+ale <- Map(ale_core, from = evaluate_at[1:(m - 1L)], to = evaluate_at[-1])
+ale <- do.call(rbind, ale)
+ale[["value"]] <- if (length(x$by)) ave(ale[["value"]], ale[[x$by]], FUN = cumsum) else
+  cumsum(ale[["value"]])
+ale
 
