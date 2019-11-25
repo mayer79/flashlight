58a381f93897a3c8413560b6610c89cf327da398
mayer79 <mayermichael79@gmail.com>
2019-11-25 21:32:48 +0100

    added interaction check

--- test_interaction.R
+++ test_interaction.R
@@ -2,22 +2,43 @@ library(iml)
 library(ggplot2)
 library(gbm)
 
-fit <- gbm(price~., data = diamonds, n.trees = 500, n.cores = 8, interaction.depth = 3, shrinkage = 0.05)
+# SITUATION: EMPIRICAL WEIGHTING IS IMPORTANT
+n <- 1000
+set.seed(326)
+data <- data.frame(
+  v = runif(n),
+  x = runif(-1, 1, n),
+  z = sample(0:1, n, replace = TRUE, prob = c(0.04, 0.96)))
+data$y <- with(data, sin(v) + x * z + rnorm(n))
 
+fit <- gbm(y ~ ., data = data, n.trees = 100, n.cores = 8,
+           interaction.depth = 5, shrinkage = 0.05)
 pf <-  function(model, newdata) {
-  predict(model, newdata, n.trees = model$n.trees)
+ predict(model, newdata, n.trees = model$n.trees)
 }
+sDat <- data#[1:50, ]
+interact.gbm(fit, data = sDat, i.var = c("x", "v")) # 0.258
+interact.gbm(fit, data = sDat, i.var = c("x", "z")) # 0.51
 
-interact.gbm(fit, data = diamonds[1:500,], i.var = c("clarity", "carat"))
 
-fl <- flashlight(model=fit, data = diamonds[1:500, ],
-                 y = "price", label ="gbm", predict_function = pf)
+fl <- flashlight(model = fit, data = sDat, y = "y", label ="lm",
+                 predict_function = pf) # 0.478
+light_interaction(fl, type = "pairwise", normalize = TRUE)$data
+# label variable  value error
+# <chr> <chr>     <dbl> <lgl>
+# 1 lm    v:x      0.122  NA
+# 2 lm    v:z      0.0635 NA
+# 3 lm    x:z      0.478  NA
 
-X <- diamonds[1:500, setdiff(colnames(diamonds), "price")]
-Y <- diamonds[1:500, "price"]
-predictor = Predictor$new(fit, data=X, y=Y, predict.fun = pf)
-Interaction$new(predictor, feature = "carat", grid.size = 9)
-Interaction$new(predictor, feature = "cut", grid.size = 9)
+X <- sDat[, setdiff(colnames(data), "y")]
+Y <- sDat[, "y"]
+predictor = Predictor$new(fit, data = X, y = Y, predict.fun = pf)
+Interaction$new(predictor, feature = "x")
+# .feature .interaction
+# 1      v:x    0.2980999
+# 2      z:x    0.5391665
 
-plot(light_interaction(fl, type = "pairwise")$data)
+FeatureImp$new(predictor, loss = mse)
+
+
 
