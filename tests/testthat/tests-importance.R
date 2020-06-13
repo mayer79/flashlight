f829f91ed7a0c89eed80fdaa703b1fc990313085
mayer79 <mayermichael79@gmail.com>
2020-06-13 15:31:25 +0200

    most_important to dplyr 1.0.0 and tests for most_important

--- tests/testthat/tests-importance.R
+++ tests/testthat/tests-importance.R
@@ -1,9 +1,21 @@
 context("importance")
 
-test_that("all_identical works", {
-  x <- list(a = 1, b = 2)
-  y <- list(a = 1, b = 3)
-  expect_true(all_identical(list(x, y), `[[`, "a"))
-  expect_false(all_identical(list(x, y), `[[`, "b"))
+fit1 <- lm(Sepal.Length ~ Petal.Width + Species + Sepal.Width, data = iris)
+fit2 <- lm(Sepal.Length ~ Petal.Width, data = iris)
+fl1 <- flashlight(model = fit1, label = "1", data = iris, y = "Sepal.Length")
+fl2 <- flashlight(model = fit2, label = "2", data = iris, y = "Sepal.Length")
+fls <- multiflashlight(list(fl1, fl2))
+
+test_that("most_important works ", {
+  imp <- light_importance(fl1, seed = 1)
+  expect_equal(most_important(imp, 1), "Species")
+  expect_equal(most_important(imp),
+               c("Species", "Sepal.Width", "Petal.Width", "Petal.Length"))
+
+  imp <- light_importance(fls, seed = 1)
+  expect_equal(most_important(imp, 1), "Petal.Width")
+  expect_equal(most_important(imp),
+               c("Petal.Width", "Species", "Sepal.Width", "Petal.Length"))
 })
 
+# TODO: Check if vignette / house-prices remain identical!
