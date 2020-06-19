f5f756dd9f95ccd272a08ff89f72c0a690a45814
mayer79 <mayermichael79@gmail.com>
2020-06-19 10:35:58 +0200

    tests for shap

--- tests/testthat/tests-shap.R
+++ tests/testthat/tests-shap.R
@@ -1,9 +1,36 @@
-context("importance")
+context("shap")
 
-test_that("all_identical works", {
-  x <- list(a = 1, b = 2)
-  y <- list(a = 1, b = 3)
-  expect_true(all_identical(list(x, y), `[[`, "a"))
-  expect_false(all_identical(list(x, y), `[[`, "b"))
+fit <- lm(Sepal.Length ~ Petal.Width, data = iris)
+fl <- flashlight(model = fit1, label = "lm", data = iris, y = "Sepal.Length")
+fl <- add_shap(fl, n_shap = 10, seed = 1)
+
+test_that("add_shap works", {
+  is.shap(fl$shap)
 })
+
+test_that("shap mode for light_importance works", {
+  imp_perm <- light_importance(fl)
+  imp_shap <- light_importance(fl, type = "shap")
+  most_important(imp_perm, 1)
+  expect_false(all(imp_perm$data$value == imp_shap$data$value))
+  expect_equal(most_important(imp_perm, 1), most_important(imp_shap, 1))
+  expect_true(inherits(plot(imp_shap), "ggplot"))
+})
+
+test_that("shap mode for light_scatter works", {
+  pred <- light_scatter(fl, v = "Petal.Width", type = "predicted")
+  shap <- light_scatter(fl, v = "Petal.Width", type = "shap")
+  expect_false(all(dim(pred$data) == dim(shap$data)))
+  expect_true(inherits(plot(shap), "ggplot"))
+
+  shap <- light_scatter(fl, v = "Sepal.Width", type = "shap")
+  expect_true(all(shap$data$value == 0))
+})
+
+test_that("shap mode for light_profile works", {
+  shap <- light_profile(fl, v = "Petal.Width", type = "shap")
+  expect_true(all(diff(shap$data$value) >= 0))
+  expect_true(inherits(plot(shap), "ggplot"))
+})
+
 
