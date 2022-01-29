c12d44fa35ea18d70a936a1a8adf75d44246c58c
mayer79 <mayermichael79@gmail.com>
2022-01-29 10:26:52 +0100

    clean up

--- packaging.R
+++ packaging.R
@@ -1,133 +1,88 @@
-#=====================================================================================
-# BUILD THE PACKAGE
-#=====================================================================================
+#=============================================================================
+# Put together the package
+#=============================================================================
 
-if (FALSE) {
-  library(MetricsWeighted)
-  library(tidyverse)
-  library(cowplot)
-  library(rpart)
-  library(rpart.plot)
-  library(testthat)
-  library(withr)
-  lapply(list.files("R", full.names = TRUE), source)
-  .onLoad()
-}
+# WORKFLOW: UPDATE EXISTING PACKAGE
+# 1) Modify package content and documentation.
+# 2) Increase package number in "use_description" below.
+# 3) Go through this script and carefully answer "no" if a "use_*" function
+#    asks to overwrite the existing files. Or just skip that function call.
 
 library(usethis)
-library(devtools)
 
-# Create a new package
-dir.create(file.path("release"))
-pkg <- file.path("release", "flashlight")
-
-create_package(
-  open = FALSE,
-  pkg,
+# Sketch of description file
+use_description(
   fields = list(
-    Title = "Shed Light on Black Box Machine Learning Models",
+    Title = "Confidence Intervals",
     Type = "Package",
-    Version = "0.8.0",
+    Version = "0.1.2",
     Date = Sys.Date(),
-    Description = "Shed light on black box machine learning models by the help of model performance, variable importance, global surrogate models, ICE profiles, partial dependence (Friedman J. H. (2001) <doi:10.1214/aos/1013203451>), accumulated local effects (Apley D. W. (2016) <arXiv:1612.08468>), further effects plots, scatter plots, interaction strength, and variable contribution breakdown (approximate SHAP) for single observations (Gosiewska and Biecek (2019) <arxiv:1903.11420>). All tools are implemented to work with case weights and allow for stratified analysis. Furthermore, multiple flashlights can be combined and analyzed together.",
-    `Authors@R` = "person('Michael', 'Mayer', email = 'mayermichael79@gmail.com', role = c('aut', 'cre', 'cph'))",
-    URL = "https://github.com/mayer79/flashlight",
-    BugReports = "https://github.com/mayer79/flashlight/issues",
-    Depends = "R (>= 3.2.0)",
-    VignetteBuilder = "knitr",
-    License = "GPL(>= 2)",
+    Description = "Calculates classic and/or bootstrap confidence intervals for many parameters such as the population mean, variance, interquartile range (IQR), median absolute deviation (MAD), skewness, kurtosis, Cramer's V, odds ratio, R-squared, quantiles (incl. median), proportions, different types of correlation measures, difference in means, quantiles and medians. Many of the classic confidence intervals are described in Smithson, M. (2003, ISBN: 978-0761924999). Bootstrap confidence intervals are calculated with the R package 'boot'. Both one- and two-sided intervals are supported.",
+    `Authors@R` = "person('Michael', 'Mayer', email = 'mayermichael79@gmail.com', role = c('aut', 'cre'))",
+    Depends = "R (>= 3.1.0)",
     LazyData = NULL,
-    Maintainer = "Michael Mayer <mayermichael79@gmail.com>")
+    Maintainer = "Michael Mayer <mayermichael79@gmail.com>"
+  ),
+  roxygen = TRUE
 )
 
-file.copy(file.path(pkg, "DESCRIPTION"), to = getwd(), overwrite = TRUE)
-# Use package has no option to look for pkg, so we first copy description from pkg, modify it and move back
+use_gpl_license(2)
+use_github_links() # use this if this project is on github
 
-# Imports
-use_package("cowplot", "Imports")
-use_package("dplyr", "Imports", min_version = "1.0.0")
-use_package("ggplot2", "Imports")
-use_package("MetricsWeighted", "Imports", min_version = "0.3.0")
-use_package("rpart", "Imports")
-use_package("rpart.plot", "Imports")
+# Your files that do not belong to the package itself (others are added by "use_* function")
+use_build_ignore(c("^packaging.R$", "[.]Rproj$", "^backlog$",
+                   "^cran-comments.md$", "^logo.png$"), escape = FALSE)
+
+# Required external packages
 use_package("stats", "Imports")
-use_package("tidyr", "Imports", min_version = "1.0.0")
-use_package("tidyselect", "Imports")
-use_package("utils", "Imports")
-use_package("withr", "Imports")
+use_package("boot", "Imports")
 
-# Suggests
-use_package("caret", "Suggests")
-use_package("knitr", "Suggests")
-use_package("mlr3", "Suggests")
-use_package("mlr3learners", "Suggests")
-use_package("moderndive", "Suggests")
-use_package("ranger", "Suggests")
-use_package("rmarkdown", "Suggests")
-use_package("testthat", "Suggests")
-use_package("xgboost", "Suggests")
+# If your code uses the pipe operator %>%
+# use_pipe()
 
-# Set up other files -------------------------------------------------
-# use_readme_md()
-# use_news_md()
-# use_cran_comments()
+# If your package contains data. Google how to document
+# use_data()
 
-# Copy readme etc.
-file.copy(c(".Rbuildignore", "NEWS.md", "README.md", "cran-comments.md", "DESCRIPTION"),
-          pkg, overwrite = TRUE)
-if (!dir.exists(file.path(pkg, "tools"))) {
-  dir.create(file.path(pkg, "tools"))
-  dir.create(file.path(pkg, "tools", "figs"))
-}
-file.copy(list.files(file.path("tools", "figs"), full.names = TRUE),
-          file.path(pkg, "tools", "figs"))
+# Add short docu in Markdown (without running R code)
+use_readme_md()
 
-# Copy R scripts and document them
-files <- list.files("R", full.names = TRUE)
-file.copy(files, file.path(pkg, "R"), overwrite = TRUE)
-devtools::document(pkg)
+# Longer docu in RMarkdown (with running R code). Often quite similar to readme.
+use_vignette("confintr")
+
+# If you want to add unit tests
+use_testthat()
+# use_test("test-cor.R")
+
+# On top of NEWS.md, describe changes made to the package
+use_news_md()
 
 # Add logo
 use_logo("logo.png")
-dir.create(file.path(pkg, "man", "figures"))
-file.copy(file.path("man", "figures", "logo.png"),
-          file.path(pkg, "man", "figures", "logo.png"))
 
-# Tests
-if (!dir.exists(file.path(pkg, "tests"))) {
-  dir.create(file.path(pkg, "tests"))
+# If package goes to CRAN: infos (check results etc.) for CRAN
+use_cran_comments()
+
+
+#=============================================================================
+# Finish package building (can use fresh session)
+#=============================================================================
+
+library(devtools)
+
+document()
+test()
+build_vignettes()
+check(manual = TRUE, cran = TRUE)
+build()
+# build(binary = TRUE)
+install()
+
+# Run only if package is public(!) and should go to CRAN
+if (FALSE) {
+  check_win_devel()
+  check_rhub()
+
+  # Wait until above checks are passed without relevant notes/warnings
+  # then submit to CRAN
+  release()
 }
-file.copy("tests", pkg, recursive = TRUE)
-test(pkg)
-
-# Copy vignette
-if (TRUE) {
-  dir.create(file.path(pkg, "vignettes"))
-  dir.create(file.path(pkg, "doc"))
-  dir.create(file.path(pkg, "Meta"))
-  file.copy(list.files("vignettes", full.names = TRUE),
-            file.path(pkg, "vignettes"), overwrite = TRUE)
-  devtools::build_vignettes(pkg)
-}
-
-# Check
-check(pkg, vignettes = FALSE)
-
-# Create
-build(pkg)
-# build(pkg, binary = TRUE)
-
-# Install
-install(pkg)
-
-# Load
-library(flashlight)
-
-
-# modify .Rbuildignore in build project to ignore the proj file.
-
-check_win_devel(pkg)
-
-check_rhub(pkg, env_vars = c(R_COMPILE_AND_INSTALL_PACKAGES = "always"))
-
-devtools::release(pkg)
--- release/flashlight/cran-comments.md
+++
@@ -1,9 +0,0 @@
-# New release 0.8.0
-
-- Major code improvements.
-- Added 2D-profile plots for partial dependence etc.
-- Improved error messages.
-- Bug fixes
-
-
-
